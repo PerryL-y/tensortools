2ed604769712a57dc5b7865288507e776c48e134
Peiyu Liu <peiyu.liu@yale.edu>
2022-06-23 20:16:00 -0400

    - re-enable parallelization for shift_cp - export multishift function

--- tensortools/cpwarp/__init__.py
+++ tensortools/cpwarp/__init__.py
@@ -1,2 +1,5 @@
 from tensortools.cpwarp.interface import ShiftedCP, fit_shifted_cp
 from tensortools.cpwarp.postprocess import shifted_align
+from tensortools.cpwarp.multishift import MultiShiftModel, multishift_hals 
+# the `multishift_hals` function from cpwarp.optimize_multishift contain functions that are non-exist
+
